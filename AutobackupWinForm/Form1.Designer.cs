efc4025fb961f7334d7bf6d4fef0249afc1fe4eb
Ayachi Gin <ayachigin@gmail.com>
2017-06-15 20:29:52 +0900

    フォームのローカライズ（というか日本語化）をした

--- AutobackupWinForm/Form1.Designer.cs
+++ AutobackupWinForm/Form1.Designer.cs
@@ -37,79 +37,113 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxExtensions = new System.Windows.Forms.TextBox();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // SourceFolderButton
             // 
-            this.SourceFolderButton.Location = new System.Drawing.Point(13, 13);
+            resources.ApplyResources(this.SourceFolderButton, "SourceFolderButton");
             this.SourceFolderButton.Name = "SourceFolderButton";
-            this.SourceFolderButton.Size = new System.Drawing.Size(24, 23);
-            this.SourceFolderButton.TabIndex = 0;
-            this.SourceFolderButton.Text = "...";
             this.SourceFolderButton.UseVisualStyleBackColor = true;
             this.SourceFolderButton.Click += new System.EventHandler(this.SourceFolderButton_Click);
             // 
             // DestFolderButton
             // 
-            this.DestFolderButton.Location = new System.Drawing.Point(13, 43);
+            resources.ApplyResources(this.DestFolderButton, "DestFolderButton");
             this.DestFolderButton.Name = "DestFolderButton";
-            this.DestFolderButton.Size = new System.Drawing.Size(24, 23);
-            this.DestFolderButton.TabIndex = 1;
-            this.DestFolderButton.Text = "...";
             this.DestFolderButton.UseVisualStyleBackColor = true;
             this.DestFolderButton.Click += new System.EventHandler(this.DestFolderButton_Click);
             // 
             // SourceFolderLabel
             // 
-            this.SourceFolderLabel.AutoSize = true;
-            this.SourceFolderLabel.Location = new System.Drawing.Point(43, 24);
+            resources.ApplyResources(this.SourceFolderLabel, "SourceFolderLabel");
             this.SourceFolderLabel.Name = "SourceFolderLabel";
-            this.SourceFolderLabel.Size = new System.Drawing.Size(108, 12);
-            this.SourceFolderLabel.TabIndex = 2;
-            this.SourceFolderLabel.Text = "Select source folder";
             // 
             // DestinationFolderLabel
             // 
-            this.DestinationFolderLabel.AutoSize = true;
-            this.DestinationFolderLabel.Location = new System.Drawing.Point(45, 53);
+            resources.ApplyResources(this.DestinationFolderLabel, "DestinationFolderLabel");
             this.DestinationFolderLabel.Name = "DestinationFolderLabel";
-            this.DestinationFolderLabel.Size = new System.Drawing.Size(130, 12);
-            this.DestinationFolderLabel.TabIndex = 3;
-            this.DestinationFolderLabel.Text = "Select destination folder";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 72);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 12);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Extension of the file to backup.";
             // 
             // textBoxExtensions
             // 
-            this.textBoxExtensions.Location = new System.Drawing.Point(13, 88);
-            this.textBoxExtensions.Multiline = true;
+            resources.ApplyResources(this.textBoxExtensions, "textBoxExtensions");
             this.textBoxExtensions.Name = "textBoxExtensions";
-            this.textBoxExtensions.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxExtensions.Size = new System.Drawing.Size(162, 161);
-            this.textBoxExtensions.TabIndex = 5;
-            this.textBoxExtensions.Text = "sai\r\nsai2\r\nlip\r\nclip\r\njpg\r\npng";
             this.textBoxExtensions.TextChanged += new System.EventHandler(this.textBoxExtensions_TextChanged);
             // 
             // notifyIcon1
             // 
             this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
-            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "Auto backup tool";
-            this.notifyIcon1.Visible = true;
+            resources.ApplyResources(this.notifyIcon1, "notifyIcon1");
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(339, 261);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBoxExtensions);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.DestinationFolderLabel);
@@ -118,7 +152,8 @@
             this.Controls.Add(this.SourceFolderButton);
             this.Name = "MainWindow";
             this.ShowInTaskbar = false;
-            this.Text = "Auto backup tool";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,6 +167,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBoxExtensions;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
---
+++ AutobackupWinForm/ImageResizer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutobackupWinForm
+{
+    class ImageResizer
+    {
+        private Bitmap img;
+
+        ImageResizer(string path)
+        {
+            try
+            {
+                img = new Bitmap(path);
+            } catch(ArgumentException e)
+            {
+                MessageBox.Show("画像をリサイズしようとしましたが失敗しました。\n" + e);
+            }
+        }
+
+        public Bitmap Resize(int longSide, bool transparentPixel)
+        {
+            var width = img.Size.Width;
+            var height = img.Size.Height;
+            var newWidth = width > height ? longSide : width / (height / longSide);
+            var newHeight = height > width ? longSide : height / (width / longSide);
+            //描画先とするImageオブジェクトを作成する
+            Bitmap canvas = new Bitmap(newWidth, newHeight);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(canvas);
+
+            g.InterpolationMode =
+                System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.DrawImage(img, 0, 0, newWidth, newHeight);
+
+            //BitmapとGraphicsオブジェクトを破棄
+            img.Dispose();
+            g.Dispose();
+
+            return canvas;
+        }
+    }
+}
